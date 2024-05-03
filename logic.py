4828a19ae2b10c431f399bc7f75d8b23e8fdc99e
MadRanger117 <167354536+MadRanger117@users.noreply.github.com>
2024-05-03 15:11:10 -0500

    Update logic.py

--- logic.py
+++ logic.py
@@ -24,14 +24,33 @@ Writes the name of the candidate in a csvfile the function creates
     def vote(self):
         with open('data.csv', mode='a', newline='') as file:
             writer = csv.writer(file)
-        pass
+            if self.cand1.isChecked():
+                writer.writerow(f'{self.candinput.text()}, str({self.candvote1.text()}))
+            elif self.cand2.isChecked():
+                writer.writerow(f'{self.candinput.text()}, str({self.candvote2.text()}))
+            elif self.cand3.isChecked():
+                writer.writerow(f'{self.candinput.text()}, str({self.candvote3.text()}))
+            elif self.cand4.isChecked():
+                writer.writerow(f'{self.candinput.text()}, str({self.candvote4.text()}))
+            elif self.cand5.isChecked():
+                writer.writerow(f'{self.candinput.text()}, str({self.candvote5.text()}))
 """
 Closes the window and showcases final totals for the votes
 """
     def exit(self):
         with open('data.csv', mode='a', newline='') as file:
             writer = csv.writer(file)
-        pass
+            if (self.cand1.text() != ''):
+                writer.writerow(f'{self.cand1.text()}, str({self.candvote1.text()}))
+            if (self.cand2.text() != ''):
+                writer.writerow(f'{self.cand2.text()}, str({self.candvote2.text()}))
+            if (self.cand3.text() != ''):
+                writer.writerow(f'{self.cand3.text()}, str({self.candvote3.text()}))
+            if (self.cand4.text() != ''):
+                writer.writerow(f'{self.cand4.text()}, str({self.candvote4.text()}))
+            if (self.cand5.text() != ''):
+                writer.writerow(f'{self.cand5.text()}, str({self.candvote5.text()}))
+        self.threadData()
 """
 Submits the name of the candiate and puts it next to a vote button
 """
