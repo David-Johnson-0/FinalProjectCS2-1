2dbeef83c3e12fdacf88dda5d7104d86950f9498
David Johnson <85799984+David-Johnson-0@users.noreply.github.com>
2024-05-10 13:45:03 -0500

    Update logic.py

--- logic.py
+++ logic.py
@@ -1,125 +1,136 @@
 from gui import *
 import csv
-import re
 from PyQt6.QtWidgets import *
-class Logic():
-"""
-Initializes the UI and ensures none of the buttons are checked by default while setting focus to the input space for candidates, num_votes are universal variables for counting votes
-"""
+class Logic(QMainWindow, Ui_VotingForm):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
 
         self.ExitButton.clicked.connect(lambda: self.exit())
-        self.SubmitButton.clicked.connect(lambda: self.submit_candidate(), self.clear())
-        self.VoteButton.clicked.connect(lambda: self.vote(), self.clear())
+        self.SubmitButton.clicked.connect(lambda: self.submit_candidate())
+        self.SubmitButton.clicked.connect(lambda: self.clear())
+        self.VoteButton.clicked.connect(lambda: self.vote())
+        self.VoteButton.clicked.connect(lambda: self.clear())
         self.cand1.setChecked(False)
         self.cand2.setChecked(False)
         self.cand3.setChecked(False)
         self.cand4.setChecked(False)
         self.cand5.setChecked(False)
         self.candinput.setFocus()
-"""
-Writes the name of the candidate in a csvfile the function creates
-"""
+        self.candidates()
+        self.infobox.setText(None)
+
     def vote(self):
-        with open('data.csv', mode='a', newline='') as file:
+        if self.cand1.isChecked():
+            name = self.cand1.text()
+        elif self.cand2.isChecked():
+            name = self.cand2.text()
+        elif self.cand3.isChecked():
+            name = self.cand3.text()
+        elif self.cand4.isChecked():
+            name = self.cand4.text()
+        elif self.cand5.isChecked():
+            name = self.cand5.text()
+        else:
+            return None
+        with open('data.csv', mode='r', newline='') as file:
+            reader = csv.reader(file)
+            rows = list(reader)
+            for row in rows:
+                if row[0] == name:
+                    row[1] = str(int(row[1]) + 1)
+
+        with open('data.csv', mode='w', newline='') as file:
             writer = csv.writer(file)
-            if self.cand1.isChecked():
-                writer.writerow(f'{self.candinput.text()}, str({self.candvote1.text()}))
-            elif self.cand2.isChecked():
-                writer.writerow(f'{self.candinput.text()}, str({self.candvote2.text()}))
-            elif self.cand3.isChecked():
-                writer.writerow(f'{self.candinput.text()}, str({self.candvote3.text()}))
-            elif self.cand4.isChecked():
-                writer.writerow(f'{self.candinput.text()}, str({self.candvote4.text()}))
-            elif self.cand5.isChecked():
-                writer.writerow(f'{self.candinput.text()}, str({self.candvote5.text()}))
-"""
-Closes the window and showcases final totals for the votes
-"""
+            writer.writerows(rows)
+        self.get_tally()
+        self.clear()
+        pass
     def exit(self):
-        with open('data.csv', mode='a', newline='') as file:
-            writer = csv.writer(file)
-            if (self.cand1.text() != ''):
-                writer.writerow(f'{self.cand1.text()}, str({self.candvote1.text()}))
-            if (self.cand2.text() != ''):
-                writer.writerow(f'{self.cand2.text()}, str({self.candvote2.text()}))
-            if (self.cand3.text() != ''):
-                writer.writerow(f'{self.cand3.text()}, str({self.candvote3.text()}))
-            if (self.cand4.text() != ''):
-                writer.writerow(f'{self.cand4.text()}, str({self.candvote4.text()}))
-            if (self.cand5.text() != ''):
-                writer.writerow(f'{self.cand5.text()}, str({self.candvote5.text()}))
-        self.threadData()
-"""
-Submits the name of the candiate and puts it next to a vote button
-"""
+        with open('data.csv', mode='r', newline='') as file:
+            reader = csv.reader(file)
+            rows = list(reader)
+            total = 0
+            winnerVal = 0
+            winnerName = None
+            for row in rows:
+                if row:
+                    current_text = self.infobox.text()
+                    add_text = f'{row[0]}...................................votes:{row[1]}'
+                    new_text = current_text + '\n' + add_text
+                    self.infobox.setText(new_text)
+                    total += int(row[1])
+                    if int(row[1]) > winnerVal:
+                        winnerVal, winnerName = int(row[1]),row[0]
+                else:
+                    continue
+            current_text = self.infobox.text()
+            self.infobox.setText(f"{current_text}\n{winnerName} wins this vote with {winnerVal} votes\n"
+                                 f" out of {total} total votes!")
+
     def submit_candidate(self):
-        if (self.cand1.text() == ''):
-            cand1.setText(f'{candinput}')
-        elif (self.cand2.text() == ''):
-            cand2.setText(f'{candinput}')        
-        elif (self.cand3.text() == ''):
-            cand3.setText(f'{candinput}')        
-        elif (self.cand4.text() == ''):
-            cand4.setText(f'{candinput}')
-        elif (self.cand5.text() == ''):
-            cand5.setText(f'{candinput}')            
-"""
-Actively gets the number of votes for each candiate
-"""
+        name = self.candinput.text()
+        if not name:
+            self.candinput.setFocus()
+        if self.cand1.text() and self.cand2.text() and self.cand3.text() and self.cand4.text() and self.cand5.text():
+            pass
+        else:
+            with open('data.csv', mode='a', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow([name, 0])
+            self.candidates()
+            self.clear()
+        pass
+
+    def candidates(self):
+        with open('data.csv', mode='r', newline='') as file:
+            reader = csv.reader(file)
+            rows = list(reader)
+            self.cand1.setText(None)
+            self.cand2.setText(None)
+            self.cand3.setText(None)
+            self.cand4.setText(None)
+            self.cand5.setText(None)
+            for row in rows:
+                if not self.cand1.text():
+                    self.cand1.setText(row[0])
+                    pass
+                elif not self.cand2.text():
+                    self.cand2.setText(row[0])
+                    pass
+                elif not self.cand3.text():
+                    self.cand3.setText(row[0])
+                    pass
+                elif not self.cand4.text():
+                    self.cand4.setText(row[0])
+                    pass
+                elif not self.cand5.text():
+                    self.cand5.setText(row[0])
+                    pass
+                else:
+                    pass
+        self.get_tally()
     def get_tally(self):
-        with open('data.csv', mode='a', newline='') as file:
-            writer = csv.writer(file)
-            if self.cand1.isChecked():
-                num_votes1 += 1
-                self.candvote1.setText(str(num_votes1))
-                for line in file:
-                    line = line.split()
-                    if re.search(f'^{cand1.text()}):
-                        del line
-                        writer.writerow(f'{self.candinput.text()}, str({self.candvote1.text()}))
-            elif self.cand2.isChecked():
-                num_votes2 += 1
-                self.candvote2.setText(str(num_votes2))
-                for line in file:
-                    line = line.split()
-                    if re.search(f'^{cand2.text()}):
-                        del line
-                        writer.writerow(f'{self.candinput.text()}, str({self.candvote2.text()}))
-            elif self.cand3.isChecked():
-                num_votes3 += 1
-                self.candvote3.setText(str(num_votes3))
-                for line in file:
-                    line = line.split()
-                    if re.search(f'^{cand3.text()}):
-                        del line
-                        writer.writerow(f'{self.candinput.text()}, str({self.candvote3.text()}))            
-            elif self.cand4.isChecked():
-                num_votes4 += 1
-                self.candvote1.setText(str(num_votes4))
-                for line in file:
-                    line = line.split()
-                    if re.search(f'^{cand4.text()}):
-                        del line
-                        writer.writerow(f'{self.candinput.text()}, str({self.candvote4.text()}))
-            elif self.cand5.isChecked():
-                num_votes5 += 1
-                self.candvote5.setText(str(num_votes5))
-                for line in file:
-                    line = line.split()
-                    if re.search(f'^{cand5.text()}):
-                        del line
-                        writer.writerow(f'{self.candinput.text()}, str({self.candvote5.text()}))
-"""
-Clears all inputs
-"""
+        with open('data.csv', mode='r', newline='') as file:
+            reader = csv.reader(file)
+            rows = list(reader)
+            for row in rows:
+                if row[0] == self.cand1.text():
+                    self.candvote1.setText(row[1])
+                elif row[0] == self.cand2.text():
+                    self.candvote2.setText(row[1])
+                elif row[0] == self.cand3.text():
+                    self.candvote3.setText(row[1])
+                elif row[0] == self.cand4.text():
+                    self.candvote4.setText(row[1])
+                elif row[0] == self.cand5.text():
+                    self.candvote5.setText(row[1])
+        pass
     def clear(self):
-        self.candinput.clear()
-        self.cand1.clear()
-        self.cand2.clear()
-        self.cand3.clear()
-        self.cand4.clear()
-        self.cand5.clear()
+        self.cand1.setChecked(False)
+        self.cand2.setChecked(False)
+        self.cand3.setChecked(False)
+        self.cand4.setChecked(False)
+        self.cand5.setChecked(False)
+        self.candinput.setText("")
         self.candinput.setFocus()
